74c694fa9a38fd32a9f0b84de09dc90687a92b13
천승준 <cheonseungjun@cheonseungjun-ui-MacBookAir.local>
2023-10-17 18:22:19 +0900

    건슬은 뒤졌다.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,8 @@ const Header = () => {
   return (
     <HeaderLayer>
       <Image src="/lost_ark_logo.webp" alt="logo" width={200} height={60} />
-      <h1>욱이가 적룡포를 쓰니 카멘의 허리가 휘었다</h1>
+      {/* <h1>욱이가 적룡포를 쓰니 카멘의 허리가 휘었다</h1> */}
+      <h1>건슬은 뒤졌다</h1>
     </HeaderLayer>
   );
 };
